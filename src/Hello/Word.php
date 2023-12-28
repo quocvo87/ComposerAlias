f159118c6e092f47ea72768480b9d6fd082a6e51
QuocVo87 <quocvo87@gmail.com>
2023-12-28 18:22:49 +0700

    init

---
+++ src/Hello/Word.php
@@ -0,0 +1,12 @@
+<?php
+namespace TrueMe\Hello
+
+class Word
+{
+    public function callSurprise()
+    {
+        var_dump('hello Word is calling to callSurprise');
+
+        return true;
+    }
+}
